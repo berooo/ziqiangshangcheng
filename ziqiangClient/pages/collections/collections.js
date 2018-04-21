090656ceeba56c527a520070170044402dabf911
Erin00 <3050035099@qq.com>
2018-04-21 21:29:53 +0800

    commit collections

---
+++ ziqiangClient/pages/collections/collections.js
@@ -0,0 +1,83 @@
+// collections.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product_list:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that=this
+    wx.getStorage({
+      key: 'userid',
+      success: function (res) {
+        wx.request({
+          url: 'http://localhost/think/index.php/Collect/display',
+          data: {
+            'userid': res.data
+          },
+          method: 'get',
+          success: function (info) {
+            that.setData({
+              product_list: info.data,
+            })
+          }
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
